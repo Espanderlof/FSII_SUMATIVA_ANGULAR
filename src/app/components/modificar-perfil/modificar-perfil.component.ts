9ff0bf4ef18998778f43d4c100e8ed79e7668768
espanderlof <jaime717@hotmail.es>
2024-06-21 16:47:04 -0400

    Creación de componentes

---
+++ src/app/components/modificar-perfil/modificar-perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './modificar-perfil.component.html',
+  styleUrl: './modificar-perfil.component.scss'
+})
+export class ModificarPerfilComponent {
+
+}
---
+++ src/app/components/registrarme/registrarme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrarme',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrarme.component.html',
+  styleUrl: './registrarme.component.scss'
+})
+export class RegistrarmeComponent {
+
+}
