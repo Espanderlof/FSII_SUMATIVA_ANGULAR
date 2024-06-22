b256cdfc21a5f99eabb503a1198f52dbc9721456
espanderlof <jaime717@hotmail.es>
2024-06-21 23:04:30 -0400

    Contacto completo

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.scss'
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit {
+  contactForm!: FormGroup;
+  submitted = false;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      celular: ['', [Validators.required, Validators.pattern(/^[0-9]{9}$/)]],
+      asunto: ['', Validators.required],
+      mensaje: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.contactForm.valid) {
+      alert('¡Mensaje enviado correctamente!');
+      this.contactForm.reset();
+      this.submitted = false;
+    }
+  }
+
+  get f() { return this.contactForm.controls; }
 }
